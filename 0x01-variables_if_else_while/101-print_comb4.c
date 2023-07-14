2701044ce58df8270f7656702210d2a4043b1002
Philip Opiyo <opiyophilipo@gmail.com>
2023-07-14 05:35:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,10 @@ int main(void)
 			g = 48;
 			while (g < 58)
 			{
-			if (e != i && e != g && i != g && e < i && i < g)
-			{
+		       
+				if (e != i && e != g && i != g && e < i && i < g)
+			
+				{
 					putchar(e);
 					putchar(i);
 					putchar(g);
