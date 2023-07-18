a2cc0494422ab01f7690d585814991067fe1f125
Philip Opiyo <opiyophilipo@gmail.com>
2023-07-18 09:32:35 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,32 @@
 #include "main.h"
 
 /**
- * jack_bauer - Print the minutes of a day
+ * jack_bauer - Prints the minutes of a day
  *
- * Return : no return
+ * Return: no return
  */
 void jack_bauer(void)
 {
-	int i = hours;
-	int j = minutes
-		for (i = 0; i < 24; i++)
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
 		{
-			for (j = 0; j < 60; j++)
+			for (c = 48; c <= 53; c++)
 			{
-				_putchar(j);
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
 			}
-			_putchar(i);
-			_putchar('\n');
 		}
+	}
 }		
