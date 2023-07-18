5b4529e4598903f33d8d3060d6f4c745322c0954
Philip Opiyo <opiyophilipo@gmail.com>
2023-07-18 09:27:07 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print the minutes of a day
+ *
+ * Return : no return
+ */
+void jack_bauer(void)
+{
+	int i = hours;
+	int j = minutes
+		for (i = 0; i < 24; i++)
+		{
+			for (j = 0; j < 60; j++)
+			{
+				_putchar(j);
+			}
+			_putchar(i);
+			_putchar('\n');
+		}
+}		
