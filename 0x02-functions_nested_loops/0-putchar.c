4edf48b75f12caa43a56e301ef78709daaec5a56
Philip Opiyo <opiyophilipo@gmail.com>
2023-07-18 06:39:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
 /**
  * Main -print _putchar and newline
@@ -11,15 +10,5 @@ int main(void)
 printf("_putchar\n");
 
 return (0);
-=======
-/**
- * main - print out _putchar()
- *
- * Return 0 :(Successfull)
- */
-int main(void){
-    printf("_putchar\n");
-    return 0;
->>>>>>> a97a8a5d947b121934573a625c13fa19de4e99ab
 }
 
