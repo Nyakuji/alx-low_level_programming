0e0ac428ba16e9abc516a15327ad3eeaf6bf4ea4
Philip Opiyo <opiyophilipo@gmail.com>
2023-07-19 06:52:46 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-/*	while (i < 10)*/
-/*	{*/
-/*		putchar(i)*/
-/*	}*/
-
-	print("Infinite loop avoided! \\o/\n");
-
-	return (0);
-}
-
-
+#include <stdio.h>                                                              
+  2                                                                                 
+  3 /**                                                                             
+  4 * main - causes an infinite loop                                                
+  5 * Return: 0                                                                     
+  6 */                                                                              
+  7                                                                                 
+  8 int main(void)                                                                  
+  9 {                                                                               
+ 10         int i;                                                                  
+ 11                                                                                 
+ 12         printf("Infinite loop incoming :(\n");                                  
+ 13                                                                                 
+ 14         i = 0;                                                                  
+ 15 /*      while (i < 10)*/                                                        
+ 16 /*      {*/                                                                     
+ 17 /*              putchar(i)*/                                                    
+ 18 /*      }*/                                                                     
+ 19                                                                                 
+ 20         print("Infinite loop avoided! \\o/\n");                                 
+ 21                                                                                 
+ 22         return (0);                                                             
+ 23 }                                                                               
+ 24                                                                                 
+ 25                       
