d0f808afd8117468dcc21e7f76c7c5dfb0abc140
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-06 05:55:38 -0700

    fixed the error

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 /**
 * main - prints all arguments it receives
 * @argc: number of command line arguments
-* argv: array that contains the program command line arguments
+* @argv: array that contains the program command line arguments
 * Return: (0) always success
 */
 int main(int argc, char *argv[])
