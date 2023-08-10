edec3ced5b0f9854284cab995c09e6360d3c7e46
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-10 13:15:47 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
-
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	unsigned int len1, len2, lenresult, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,17 +21,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n >= len2)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (n > len2)
 		n = len2;
 
-	result = (char *)malloc(len1 + n + 1);
+	lenresult = len1 + n;
+
+	result = malloc(lenresult + 1);
+
 	if (result == NULL)
-	{
 		return (NULL);
-	}
-	strncpy(result, s1, len1);
-	strncpy(result + len1, s2, n);
-	result[len1 + n] = '\0';
+
+	for (i = 0; i < lenresult; i++)
+		if (i < len1)
+			result[i] = s1[i];
+		else
+			result[i] = s2[i - len1];
+
+	result[i] = '\0';
 
 	return (result);
 }
