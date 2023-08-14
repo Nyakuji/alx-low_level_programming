459141bc4356e0fa6243dc92c04b0d82e6223925
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-14 07:50:53 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	newDog =(dog_t *)malloc(sizeof(dog_t));
+	newDog = (dog_t *)malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
 
