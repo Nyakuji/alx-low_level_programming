2c1b540906356168c4a524b1e77cdfded46b502c
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-24 12:27:09 -0700

    more description

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,17 @@
 section .data
-    hello_message db "Hello, Holberton",0
-    format db "%s", 10, 0  ; "%s" format with newline (10) and null terminator (0)
+    msg db "Hello, Holberton", 0  ; Null-terminated string
+    fmt db "%s", 10, 0           ; Format string: "%s" followed by newline and null terminator
 
 section .text
+    extern printf
     global main
 
-extern printf
-
 main:
-    push rdi             ; Preserve registers
-    push rsi
-    push rax
+    mov esi, msg    ; Load the address of the message into ESI
+    mov edi, fmt    ; Load the address of the format string into EDI
+    xor eax, eax    ; Clear EAX register (set it to 0)
+    call printf     ; Call the printf function
 
-    lea rdi, [hello_message] ; Load address of the hello_message string
-    lea rsi, [format]        ; Load address of the format string
-    call printf             ; Call printf with the strings as arguments
+    xor eax, eax    ; Clear EAX register again (optional)
+    ret             ; Return from the main function
 
-    pop rax              ; Restore registers
-    pop rsi
-    pop rdi
-
-    xor rax, rax         ; Return 0
-    ret
