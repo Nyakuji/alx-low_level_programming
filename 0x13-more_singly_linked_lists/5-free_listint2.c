2f5c5981767f0b467c7fb3b88d1d6bebca842e18
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-28 12:18:06 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a listint_t list.
+* @head: head of the list.
+*
+* Return: no return.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL; /* set the head to NULL to indicate an empty list */
+}
