4df50aeb4f5fb931a15569f563b00ad7c1f03d45
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-28 09:50:44 -0700

    fixed error!

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _lISTS_
-#define _LISTS_
+#ifndef _LISTS_
+#define  _LISTS_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -30,4 +30,4 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-#endif 
+#endif
