f918192a958bb7325faf6c6abc0c00bd69a6995e
Philip Opiyo <opiyophilipo@gmail.com>
2023-09-04 11:45:44 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,27 +23,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	if (buffer == NULL)
 		return (0);
 
 	bytesRead = read(fd, buffer, letters);
-
 	if (bytesRead == -1)
 	{
-		close(fd);
 		free(buffer);
 		return (0);
 	}
 
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-
 	if (bytesWritten == -1)
 	{
-		close(fd);
 		free(buffer);
 		return (0);
 	}
-	close (fd);
+	close(fd);
 	free(buffer);
 	return (bytesWritten);
 }
