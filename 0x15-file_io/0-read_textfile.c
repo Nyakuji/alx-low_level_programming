5842d2b9262bd05f2d80402ffd4a485a12742b8b
Philip Opiyo <opiyophilipo@gmail.com>
2023-09-04 10:50:41 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "stdlib.h"
 
 /**
 * read_textfile - Reads a text file and prints it to the POSIX standard
@@ -15,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytesRead, bytesWritten;
 	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -28,11 +27,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytesRead = read(fd, buffer, letters);
+
+	if (bytesRead == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 
-	close(fd);
-
+	if (bytesWritten ==  -1 || bytesWritten != bytesRead)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close (fd);
 	free(buffer);
-
 	return (bytesWritten);
 }
