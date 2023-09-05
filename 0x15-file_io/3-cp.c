c10d1b50caf0c9ffbb2c8fbb797760b5c88b843d
Philip Opiyo <opiyophilipo@gmail.com>
2023-09-05 09:50:49 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 	}
 	if (close(from_fd) == -1 || close(to_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n");
 		exit(100);
 	}
 	return (0);
