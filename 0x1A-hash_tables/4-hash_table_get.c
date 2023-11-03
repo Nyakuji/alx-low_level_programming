3025d3df84a72aadf69d411e8a00811ab33ac1fd
Philip Opiyo <opiyophilipo@gmail.com>
2023-11-03 06:24:48 -0700

    some fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -22,6 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(current->key, key) == 0)
 			return (current->value);
+		current = current->next;
 	}
 
 	return (NULL);
